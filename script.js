251803061a40b5623429422eef640257bcbf9369
Mou41 <162114887+Mou41@users.noreply.github.com>
2024-03-04 17:20:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,20 @@ document.addEventListener('DOMContentLoaded', function () {
     const searchInput = document.getElementById('searchInput');
     const products = document.querySelectorAll('.product');
 
-    // Function to toggle additional information display
-    function toggleAdditionalInfo(event) {
+    // Function to toggle all content display
+    function toggleAllContent(event) {
         const product = event.currentTarget;
         const additionalInfo = product.querySelector('.additional-info');
+        const productName = product.querySelector('h3');
+
+        // Toggle the display of additional info and price
         additionalInfo.style.display = additionalInfo.style.display === 'block' ? 'none' : 'block';
+        productName.style.display = productName.style.display === 'block' ? 'none' : 'block';
     }
 
-    // Event listeners for toggling additional information
+    // Event listeners for toggling all content
     products.forEach(product => {
-        product.addEventListener('click', toggleAdditionalInfo);
+        product.addEventListener('click', toggleAllContent);
     });
 
     // Event listener for search input
